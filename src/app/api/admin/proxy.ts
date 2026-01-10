21854d0d001e3f6576746a58eed049a46d37617f
GasyCoder <bezaraflorent@gmail.com>
2026-01-10 19:33:07 +0300

    update front

--- src/app/api/admin/proxy.ts
+++ src/app/api/admin/proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const token = req.cookies.get("umg_admin_token")?.value;
 
   const isAdminPath = req.nextUrl.pathname.startsWith("/admin");
